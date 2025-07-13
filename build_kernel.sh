3f00be2b50790632a303ffc4dfef0884b71155f2
Gen 155 <renvelations25@gmail.com>
2025-07-13 01:58:31 +0000

    set compile with clang 14

--- build_kernel.sh
+++ build_kernel.sh
@@ -1,8 +1,11 @@
 #!/bin/bash
+# clang 14
 
-export PATH=$(pwd)/toolchain/clang/host/linux-x86/clang-r383902/bin:$PATH
-export CROSS_COMPILE=$(pwd)/toolchain/clang/host/linux-x86/clang-r383902/bin/aarch64-linux-gnu-
-export CC=$(pwd)/toolchain/clang/host/linux-x86/clang-r383902/bin/clang
+git clone https://gitlab.com/mcdofrenchfreis/android-clang-14.0.7.git toolchain
+
+export PATH=toolchain/bin:$PATH
+export CROSS_COMPILE=toolchain/bin/aarch64-linux-gnu-
+export CC=toolchain/bin/clang
 export CLANG_TRIPLE=aarch64-linux-gnu-
 export ARCH=arm64
 export ANDROID_MAJOR_VERSION=r
